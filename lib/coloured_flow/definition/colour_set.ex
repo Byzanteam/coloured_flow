26714735babb67e116aae4ac9ca360467e75b7ef
Phil Chen <06fahchen@gmail.com>
2024-07-24 11:47:34 +0800

    feat: add Of to check the type of a term

--- lib/coloured_flow/definition/colour_set.ex
+++ lib/coloured_flow/definition/colour_set.ex
@@ -5,6 +5,12 @@ defmodule ColouredFlow.Definition.ColourSet do
   use TypedStructor
 
   @type name() :: atom()
+
+  @typep primitive_value() ::
+           integer()
+           | boolean()
+           | float()
+           | binary()
   @typedoc """
   The value should be a literal quoted expression.
 
@@ -21,14 +27,45 @@ defmodule ColouredFlow.Definition.ColourSet do
       iex> %{name: "Alice", age: age}
       iex> [1, 2, number]
   """
-  @type value() :: Macro.t()
+
+  @type value() ::
+          primitive_value()
+          # unit
+          | {}
+          # tuple
+          | tuple()
+          # map
+          | map()
+          # enum
+          | atom()
+          # union
+          | {tag :: atom(), value()}
+          # list
+          | [value()]
+
+  @typep primitive_descr() :: {:integer, []} | {:float, []} | {:boolean, []} | {:binary, []}
+  @typep unit_descr() :: {:unit, []}
+  @typep tuple_descr() :: {:tuple, [descr()]}
+  @typep map_descr() :: {:map, %{atom() => descr()}}
+  @typep enum_descr() :: {:enum, [atom()]}
+  @typep union_descr() :: {:union, %{(tag :: atom()) => descr()}}
+  @typep list_descr() :: {:list, descr()}
+
+  @type descr() ::
+          primitive_descr()
+          | unit_descr()
+          | tuple_descr()
+          | map_descr()
+          | enum_descr()
+          | union_descr()
+          | list_descr()
 
   typed_structor enforce: true do
     plugin TypedStructor.Plugins.DocFields
 
     field :name, name()
 
-    field :type, Macro.t(),
+    field :type, descr(),
       doc: "The type of the colour set, see module documentation for more information."
   end
 end
---
+++ lib/coloured_flow/definition/colour_set/of.ex
@@ -0,0 +1,62 @@
+defmodule ColouredFlow.Definition.ColourSet.Of do
+  @moduledoc """
+  Functions to check the colour set.
+  """
+
+  alias ColouredFlow.Definition.ColourSet
+
+  @doc """
+  Check if the value is of the type.
+  """
+  # credo:disable-for-next-line JetCredo.Checks.ExplicitAnyType
+  @spec of_type(value :: term(), descr :: ColourSet.descr()) :: {:ok, ColourSet.value()} | :error
+  def of_type(value, descr) do
+    if type?(value, descr) do
+      {:ok, value}
+    else
+      :error
+    end
+  end
+
+  # primitive
+  defp type?(int, {:integer, []}) when is_integer(int), do: true
+  defp type?(float, {:float, []}) when is_float(float), do: true
+  defp type?(bool, {:boolean, []}) when is_boolean(bool), do: true
+  defp type?(binary, {:binary, []}) when is_binary(binary), do: true
+
+  defp type?({}, {:unit, []}), do: true
+
+  # composite
+  defp type?(tuple, {:tuple, types})
+       when is_tuple(tuple) and tuple_size(tuple) === length(types) do
+    values = Tuple.to_list(tuple)
+
+    Enum.all?(Enum.zip(values, types), &type?(elem(&1, 0), elem(&1, 1)))
+  end
+
+  defp type?(map, {:map, types}) when is_map(map) and map_size(map) === map_size(types) do
+    Enum.all?(map, fn {key, value} ->
+      case Map.fetch(types, key) do
+        :error -> false
+        {:ok, type} -> type?(value, type)
+      end
+    end)
+  end
+
+  defp type?(enum, {:enum, items}) when is_atom(enum) do
+    enum in items
+  end
+
+  defp type?({tag, value}, {:union, types}) when is_atom(tag) do
+    case Map.fetch(types, tag) do
+      :error -> false
+      {:ok, type} -> type?(value, type)
+    end
+  end
+
+  defp type?(list, {:list, type}) when is_list(list) do
+    Enum.all?(list, &type?(&1, type))
+  end
+
+  defp type?(_value, _type), do: false
+end
