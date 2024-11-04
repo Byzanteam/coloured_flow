bd6036918c1326b8beafea362a58bef7749c3462
Phil Chen <06fahchen@gmail.com>
2024-11-04 19:40:59 +0800

    feat: expose start_enactment and transition apis (#20)

--- lib/coloured_flow/runner/runner.ex
+++ lib/coloured_flow/runner/runner.ex
@@ -3,8 +3,17 @@ defmodule ColouredFlow.Runner do
   The ColouredFlow runner.
   """
 
+  alias ColouredFlow.Runner.Enactment.Supervisor, as: EnactmentSupervisor
   alias ColouredFlow.Runner.Enactment.WorkitemTransition
+
+  defdelegate start_enactment(enactment_id), to: EnactmentSupervisor
 
   defdelegate allocate_workitem(enactment_id, workitem_id), to: WorkitemTransition
   defdelegate allocate_workitems(enactment_id, workitem_ids), to: WorkitemTransition
+
+  defdelegate start_workitem(enactment_id, workitem_id), to: WorkitemTransition
+  defdelegate start_workitems(enactment_id, workitem_ids), to: WorkitemTransition
+
+  defdelegate complete_workitem(enactment_id, workitem_id_and_outputs), to: WorkitemTransition
+  defdelegate complete_workitems(enactment_id, workitem_ids_and_outputs), to: WorkitemTransition
 end
