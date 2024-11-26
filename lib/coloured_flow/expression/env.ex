49526fc821e1a97aa997e1353bc0516db68ce347
Phil Chen <06fahchen@gmail.com>
2024-11-26 16:13:07 +0800

    fixup! feat: add terminition criteria to check criteria for terminating enactments (#27)

--- lib/coloured_flow/expression/env.ex
+++ lib/coloured_flow/expression/env.ex
@@ -9,14 +9,12 @@ defmodule ColouredFlow.Expression.Env do
   ## Env
 
   1. `import ColouredFlow.Expression.Arc, only: [bind: 1]`
-  2. `import ColouredFlow.MultiSet, only: :macros`
-  3. `import ColouredFlow.MultiSet, only: :sigils`
+  2. `import ColouredFlow.MultiSet, only: [multi_set_coefficient: 2, sigil_MS: 2]`
   """
   @spec make_env() :: Macro.Env.t()
   def make_env do
     import ColouredFlow.Expression.Arc, only: [bind: 1], warn: false
-    import ColouredFlow.MultiSet, only: :macros, warn: false
-    import ColouredFlow.MultiSet, only: :sigils, warn: false
+    import ColouredFlow.MultiSet, only: [multi_set_coefficient: 2, sigil_MS: 2], warn: false
 
     __ENV__
   end
