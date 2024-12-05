eb7ca6a22bf17b1462c3eb6cdbcb1b93952bbc03
Phil Chen <06fahchen@gmail.com>
2024-12-05 20:42:10 +0800

    chore: update to elixir 1.17

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule ColouredFlow.MixProject do
     [
       app: :coloured_flow,
       version: @version,
-      elixir: "~> 1.16",
+      elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       elixirc_paths: elixirc_paths(Mix.env()),
