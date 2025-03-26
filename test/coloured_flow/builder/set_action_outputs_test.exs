8b563fcea3a614b5bae9d1cfe299b22e23aab678
Phil Chen <06fahchen@gmail.com>
2025-03-26 18:34:17 +0800

    fix: constrain variables in transition arcs (#65)

--- test/coloured_flow/builder/set_action_outputs_test.exs
+++ test/coloured_flow/builder/set_action_outputs_test.exs
@@ -23,23 +23,35 @@ defmodule ColouredFlow.Builder.SetActionOutputsTest do
       ],
       places: [
         %Place{name: "input", colour_set: :int},
+        %Place{name: "intermediate", colour_set: :int},
         %Place{name: "output", colour_set: :int}
       ],
       transitions: [
-        build_transition!(name: "pass_through")
+        build_transition!(name: "pass_through_1"),
+        build_transition!(name: "pass_through_2")
       ],
       arcs: [
         build_arc!(
-          label: "in",
           place: "input",
-          transition: "pass_through",
+          transition: "pass_through_1",
           orientation: :p_to_t,
           expression: "bind {1, x}"
         ),
         build_arc!(
-          label: "out",
+          place: "intermediate",
+          transition: "pass_through_1",
+          orientation: :t_to_p,
+          expression: "{1, x}"
+        ),
+        build_arc!(
+          place: "intermediate",
+          transition: "pass_through_2",
+          orientation: :p_to_t,
+          expression: "bind {1, zero}"
+        ),
+        build_arc!(
           place: "output",
-          transition: "pass_through",
+          transition: "pass_through_2",
           orientation: :t_to_p,
           expression: "{1, x}"
         )
@@ -50,9 +62,10 @@ defmodule ColouredFlow.Builder.SetActionOutputsTest do
   end
 
   test "works", %{cpnet: cpnet} do
-    assert %{transitions: [transition]} = SetActionOutputs.run(cpnet)
+    assert %{transitions: [transition_1, transition_2]} = SetActionOutputs.run(cpnet)
 
-    assert [] === get_in(transition, [Access.key(:action), Access.key(:outputs)])
+    assert [] === get_in(transition_1, [Access.key(:action), Access.key(:outputs)])
+    assert [:x] === get_in(transition_2, [Access.key(:action), Access.key(:outputs)])
   end
 
   test "sets outputs", %{cpnet: cpnet} do
@@ -60,6 +73,7 @@ defmodule ColouredFlow.Builder.SetActionOutputsTest do
       arcs: [
         %Arc{orientation: :p_to_t} = input_arc,
         %Arc{orientation: :t_to_p}
+        | rest_arcs
       ]
     } = cpnet
 
@@ -67,15 +81,15 @@ defmodule ColouredFlow.Builder.SetActionOutputsTest do
       Map.put(cpnet, :arcs, [
         input_arc,
         build_arc!(
-          label: "out",
-          place: "output",
-          transition: "pass_through",
+          place: "intermediate",
+          transition: "pass_through_1",
           orientation: :t_to_p,
           expression: "{x, y}"
         )
+        | rest_arcs
       ])
 
-    assert %{transitions: [transition]} = SetActionOutputs.run(cpnet)
+    assert %{transitions: [transition | _rest]} = SetActionOutputs.run(cpnet)
 
     assert [:y] === get_in(transition, [Access.key(:action), Access.key(:outputs)])
   end
@@ -85,6 +99,7 @@ defmodule ColouredFlow.Builder.SetActionOutputsTest do
       arcs: [
         %Arc{orientation: :p_to_t} = input_arc,
         %Arc{orientation: :t_to_p}
+        | rest_arcs
       ]
     } = cpnet
 
@@ -92,15 +107,15 @@ defmodule ColouredFlow.Builder.SetActionOutputsTest do
       Map.put(cpnet, :arcs, [
         input_arc,
         build_arc!(
-          label: "out",
-          place: "output",
-          transition: "pass_through",
+          place: "intermediate",
+          transition: "pass_through_1",
           orientation: :t_to_p,
           expression: "{zero, y}"
         )
+        | rest_arcs
       ])
 
-    assert %{transitions: [transition]} = SetActionOutputs.run(cpnet)
+    assert %{transitions: [transition | _rest]} = SetActionOutputs.run(cpnet)
 
     assert [:y] === get_in(transition, [Access.key(:action), Access.key(:outputs)])
   end
